a75bbbc9e0c5d5195b047660bc8c2e0791afdaf7
Aadit3003 <82210227+Aadit3003@users.noreply.github.com>
2022-02-24 08:48:44 +0530

    Implemented getNextToken()

---
+++ copy.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+
+//helper functions
+
+//strip white space from beginning of string
+char *strip(char *str)
+{
+    while(*str==' ') str++;
+    return str;
+}
+
+void removeComments(char* testcaseFile, char* cleanFile){
+    printf("%s\n",testcaseFile);
+    FILE *inp = fopen(testcaseFile,"r");
+    FILE *out = fopen(cleanFile,"w");
+
+    if(!inp){
+        printf("Error opening testcaseFile\n");
+        return;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t sizeRead;
+
+    sizeRead = getline(&line, &len, inp);
+
+    while(sizeRead != -1) {
+        char *mod = strip(line);
+        if(strlen(mod)>0){
+            if(mod[0]=='%'){
+                sizeRead = getline(&line, &len, inp);
+                continue;
+            }
+        }
+        fprintf(out,"%s",line);
+        sizeRead = getline(&line, &len, inp);
+    }
+}
+
+tokenInfo getNextToken(twinBuffer B){
+    
+}
+
+void main(){
+    removeComments("testComments.txt","output.txt");
+}
