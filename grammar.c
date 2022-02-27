a922d45ef91be27b1f6ffe000b399b3e15343caf
Aadit3003 <82210227+Aadit3003@users.noreply.github.com>
2022-02-27 15:54:11 +0530

    Update grammar.c

--- grammar.c
+++ grammar.c
@@ -28,22 +28,10 @@ g_Node* insertNode(g_Node* head, int i){
 int main(){
 	gterm trial = 68;
 
-	// printf("Hi\n");
-	// printToken(trial);
-	// printf("\n");
-
 	Grammar G;
 	G[0].symbol = 54;
 	G[0].head = insertNode(G[0].head, 68);
 	G[0].head = insertNode(G[0].head, 89);
-	// G[0].symbol = 68;
-	// G[0].head = (g_Node*)malloc(sizeof(g_Node));
-
-	// G[0].head->symbol = 76;
-	// g_Node* temp = (g_Node*)malloc(sizeof(g_Node));
-	// temp->symbol = 88;
-
-	// G[0].head->next = temp;
 
 	printRule(G[0]);
 }
