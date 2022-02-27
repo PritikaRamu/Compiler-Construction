33c592269a454d976d05ed283308a636a3223aac
Aadit3003 <82210227+Aadit3003@users.noreply.github.com>
2022-02-27 15:50:29 +0530

    Grammar Insertion

--- grammar.c
+++ grammar.c
@@ -8,12 +8,21 @@
 void printRule(g_cell lhs){
 	printToken(lhs.symbol);
 	printf("->");
-	g_Node h = lhs.head[0];
-	while(h.next!=NULL){
-		printToken(h.symbol);
+	g_Node* h = lhs.head;
+	while(h!=NULL){
+		printToken(h->symbol);
+		h = h->next;
 	}
 	printf("\n");
 
+}
+
+g_Node* insertNode(g_Node* head, int i){
+	g_Node* temp = (g_Node*)malloc(sizeof(g_Node));
+	temp->symbol = i;
+
+	temp->next = head;
+	return temp;
 }
 
 int main(){
@@ -24,13 +33,18 @@ int main(){
 	// printf("\n");
 
 	Grammar G;
-	G[0].symbol = 68;
-	G[0].head = (g_Node*)malloc(sizeof(g_Node*));
+	G[0].symbol = 54;
+	G[0].head = insertNode(G[0].head, 68);
+	G[0].head = insertNode(G[0].head, 89);
+	// G[0].symbol = 68;
+	// G[0].head = (g_Node*)malloc(sizeof(g_Node));
 
-	(G[0].head[0]).symbol = 76;
-	
-	printf(sizeof(G[0].head));
-	printf("\n");
+	// G[0].head->symbol = 76;
+	// g_Node* temp = (g_Node*)malloc(sizeof(g_Node));
+	// temp->symbol = 88;
+
+	// G[0].head->next = temp;
 
 	printRule(G[0]);
 }
+
