e36698e2ddc5acb5767ae9d0c5a4ddb7b29b74e3
Aadit3003 <82210227+Aadit3003@users.noreply.github.com>
2022-03-01 11:54:41 +0530

    Perfect Grammar Init
    Updated Non-terminals, printToken(), printNonTerminal() and populated global Grammar variable

--- gterm.h
+++ gterm.h
@@ -4,18 +4,22 @@ typedef enum {
 	parameter_list, dataType, primitiveDatatype, constructedDatatype,
 	remaining_list, stmts, typeDefinitions, typeDefinition, fieldDefinitions,
 	fieldDefinition, moreFields, declarations, declaration, global_or_not,
-	otherStmts, stmt, assignmentStmt, singleOrRecId, new_24, funCallStmt,
+	otherStmts, stmt, assignmentStmt, singleOrRecId, actualOrRedefined, funCallStmt,
 	outputParameters, inputParameters, iterativeStmt, conditionalStmt, elsePart,
-	ioStmt, allVar, allVarTemp, arithmeticExpression, expPrime, term, termPrime, factor,
-	highPrecedenceOperators, lowPrecedenceOperators, all, temp, booleanExpression,
-	var, logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids, eps,
-	TK_ASSIGNOP, TK_COMMENT, TK_FIELDID, TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_TYPE, TK_MAIN,
+	ioStmt, fieldtype, option_single_constructed, arithmeticExpression, expPrime, term, termPrime, factor,
+	highPrecedenceOperators, lowPrecedenceOperators, oneExpansion, moreExpansions, booleanExpression,
+	var, logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids,
+	eps, definetypestmt, A, TK_ASSIGNOP, TK_COMMENT, TK_FIELDID, TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_TYPE, TK_MAIN,
 	TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_SQL, TK_SQR, TK_INPUT, TK_OUTPUT, TK_INT,
 	TK_REAL, TK_COMMA, TK_SEM, TK_COLON, TK_DOT, TK_ENDWHILE, TK_OP, TK_CL, TK_IF,
 	TK_THEN, TK_ENDIF, TK_READ, TK_WRITE, TK_RETURN, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV,
 	TK_CALL, TK_RECORD, TK_ENDRECORD, TK_ELSE, TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_AS,
 	TK_EQ, TK_GT, TK_GE, TK_NE, SENTINEL, LEX_ERROR, TK_RUID, TK_UNION, TK_ENDUNION, TK_DEFINETYPE, $
 } g_Term;
+
+// Delete allVar, allVarTemp, all, temp
+// Add actualOrRedefined, fieldtype, option_single_constructed, oneExpansion, moreExpansions
+// Change Numbers!!! (definetypestmt, A)
 
 // typedef enum {
 // 	program, mainFunction, otherFunctions, function, input_par, output_par,
